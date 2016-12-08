a299db1442306b128850fa067d6f666f339a2427
Eduardo Rivas <e@jerivas.com>
2016-12-08 14:32:58 -0600

    Mixing: Enable Preact devtools in development.

--- mixing/frontend/src/index.js
+++ mixing/frontend/src/index.js
@@ -3,6 +3,8 @@ import { Provider } from 'preact-redux';
 import store from './store';
 import App from './components/app';
 
+if (module.hot) require('preact/devtools');
+
 render((
 	<Provider store={store}>
 		<App />
