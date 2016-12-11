1b5bc433f8156eac3130e94139f4f16e7aee4aef
Eduardo Rivas <e@jerivas.com>
2016-12-11 17:49:51 -0600

    Mixing: Move reducers to reducers.js.

--- mixing/frontend/app/reducers.js
+++ mixing/frontend/app/reducers.js
@@ -1,6 +1,22 @@
 
-const EMPTY = {};
+const ACTIONS = {
+	ADD_TODO: ({ todos, ...state }, { text }) => ({
+		todos: [...todos, {
+			id: Math.random().toString(36).substring(2),
+			text
+		}],
+		...state
+	}),
 
-export default store => {
-	return store || EMPTY;
+	REMOVE_TODO: ({ todos, ...state }, { todo }) => ({
+		todos: todos.filter( i => i!==todo ),
+		...state
+	})
 };
+
+export default (state, action) => {
+	if (action && ACTIONS[action.type]) {
+		return ACTIONS[action.type](state, action)
+	}
+	return state;
+}
--- mixing/frontend/app/store.js
+++ mixing/frontend/app/store.js
@@ -1,24 +1,8 @@
 import { createStore } from 'redux';
-
-let ACTIONS = {
-	ADD_TODO: ({ todos, ...state }, { text }) => ({
-		todos: [...todos, {
-			id: Math.random().toString(36).substring(2),
-			text
-		}],
-		...state
-	}),
-
-	REMOVE_TODO: ({ todos, ...state }, { todo }) => ({
-		todos: todos.filter( i => i!==todo ),
-		...state
-	})
-};
+import rootReducer from './reducers';
 
 const INITIAL = {
 	todos: []
 };
 
-export default createStore( (state, action) => (
-	action && ACTIONS[action.type] ? ACTIONS[action.type](state, action) : state
-), INITIAL, window.devToolsExtension && window.devToolsExtension());
+export default createStore(rootReducer, INITIAL, window.devToolsExtension && window.devToolsExtension());
