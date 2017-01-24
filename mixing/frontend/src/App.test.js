c05d549db199d60e6c174e6812fe8919cae88ac7
Eduardo Rivas <e@jerivas.com>
2017-01-23 22:56:12 -0600

    Mixing: Delete old boilerplate files.

--- mixing/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, h } from 'preact';
-import App from './App';
-
-/** @jsx h */
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
-});
